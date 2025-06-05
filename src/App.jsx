9fcad09469d492675e67e760ec7c8a9f572196bc
githubdivyeshmpatil <githubdivyeshmpatil@gmail.com>
2025-06-05 18:46:26 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import LEDPage from "./RouterProduct/LEDPage";
@@ -158,7 +156,7 @@ function App() {
           <Route path="/consumer-electronics/geysers" element={<Geysers />} />
         </Routes>
       </Router>
-      <Footer />
+    
     </>
   );
 }
