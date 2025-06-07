96c22247fd7f98c80210245a44fdfa044dc103dd
githubdivyeshmpatil <githubdivyeshmpatil@gmail.com>
2025-06-07 17:04:35 +0530

    hu

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import BackgroundSection from '../components/BackgroundSection'
 
 function Contact() {
+   useEffect(() => {
+      window.scrollTo(0, 0);
+    }, []);
   return (
     <>
       <Navbar />
